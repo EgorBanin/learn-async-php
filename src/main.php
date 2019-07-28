c8c820ddabd2d27e9774e86b10af915ed8c167cd
Egor Banin <gbanin@ctcmedia.ru>
2019-07-28 14:44:27 +0300

    немного пояснений

--- src/main.php
+++ src/main.php
@@ -1,8 +1,13 @@
 <?php declare(strict_types=1);
 
+$address = '127.0.0.1';
+$port = 8080;
+
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_bind($socket, '127.0.0.1', 8080);
+socket_bind($socket, $address, $port);
 socket_listen($socket);
+
+echo $address . ':'. $port . "\n";
 
 while (true) {
     $connection = socket_accept($socket);
